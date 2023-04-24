3d9c89dfeb959f917012ce6494898f7cfdf9d2be
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-04-24 18:11:40 -0400

    File Recovery

--- Music/apps.py
+++ Music/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BoontuneConfig(AppConfig):
+class MusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'BoonTune'
+    name = 'Music'
